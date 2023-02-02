77fd93818a431c271cff1e5cfd885170af7fa606
MayankSG <mayanksaraf04@gmail.com>
2023-02-02 16:45:58 +0530

    user authentication api done

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const globalErrorHandler = require("./controllers/errorController");
+
+/**
+ * Router files
+ */
+const userRouter = require("./routes/userRoutes");
 
 // Start express app
 const app = express();
 
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+// 3) ROUTES
+app.use("/api/v1/users", userRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
